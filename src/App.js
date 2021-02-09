c9a7a315554b5308c68c120ae0145cb04155b534
Hans-Eric Lippke <helippke@betteru.ca>
2021-02-09 17:36:34 -0500

    Using react-chat-ui package
    Using a package to manage chat ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import { AmplifyAuthenticator, AmplifySignOut, AmplifySignIn } from '@aws-amplify/ui-react'
 import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { useHistory } from "react-router"
 
 import Container from './components/Container'
 
 const App = () => {
   const [authState, setAuthState] = useState();
   const [user, setUser] = useState();
+  //let history = useHistory();
 
   useEffect(() => {
       return onAuthUIStateChange((nextAuthState, authData) => {
             setAuthState(nextAuthState);
             setUser(authData)
+
+            //if (nextAuthState === AuthState.SignedIn) {
+          //      history.push("/dashboard");
+            //}
         });
+    //}, [history]);
     }, []);
 
   return authState === AuthState.SignedIn && user ? (
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, setState } from 'react';
 
 import { Auth } from 'aws-amplify'
 import API, { graphqlOperation } from '@aws-amplify/api';
@@ -8,12 +8,15 @@ import { createMessage } from '../graphql/mutations';
 import { onCreateMessage } from '../graphql/subscriptions';
 import { messagesByChannelId } from '../graphql/queries';
 
+import { ChatFeed, Message } from 'react-chat-ui';
+
 import './Chat.css';
 import Container from './Container'
 
 function Chat(props) {
   const [messages, setMessages] = useState([]);
   const [messageBody, setMessageBody] = useState('');
+
   //check if logged in
   useEffect(() => {
     Auth.currentAuthenticatedUser()
@@ -32,6 +35,7 @@ function Chat(props) {
         .then((response) => {
           const items = response?.data?.messagesByChannelID?.items;
 
+          console.log(items);
           if (items) {
             setMessages(items);
           }
@@ -81,6 +85,57 @@ function Chat(props) {
   return (
     <Container>
       <div className="container">
+      <ChatFeed
+          chatBubble={false}
+          messages={ messages.map(
+              message =>
+                new Message({
+                  id: message.author === 'Dave' ? 0 : message.author,
+                  senderName: message.author,
+                  message: message.body,
+                })
+          )
+          } // Array: list of message objects
+          isTyping={false} // Boolean: is the recipient typing
+          hasInputField={false} // Boolean: use our input, or use your own
+          showSenderName // show the name of the user who sent the message
+          bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+          // JSON: Custom bubble styles
+          bubbleStyles={
+            {
+              text: {
+                fontSize: '1.2em',
+                fontWeight: 400,
+              },
+              chatbubble: {
+                borderRadius: 12,
+                paddingTop: 8,
+                paddingBottom: 8,
+                paddingLeft:15,
+                paddingRight:15,
+                marginTop: 5
+              }
+            }
+          }
+        />
+        <div className="chat-bar">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="message"
+              placeholder="Type your message here..."
+              onChange={handleChange}
+              value={messageBody}
+            />
+          </form>
+        </div>
+      </div>
+    </Container>
+  );
+  /*
+  return (
+    <Container>
+      <div className="container">
         <div className="messages">
           <div className="messages-scroller">
             { messages.map((message) => (
@@ -101,7 +156,7 @@ function Chat(props) {
         </div>
       </div>
     </Container>
-  );
+  );*/
 };
 
 export default Chat;
